87eab755f5342f791e81c7743e6fc29f0488dda2
Meli <melidragon@gmail.com>
2023-04-11 10:34:46 +0300

    Advanced and Revert Status methods in Feedback ar implemented

--- TaskManager/TaskManager/Models/Feedback.cs
+++ TaskManager/TaskManager/Models/Feedback.cs
@@ -15,6 +15,14 @@ namespace TaskManager.Models
         private const int RatingMinValue = 0;
         private const int RatingMaxValue = 10;
         private int rating;
+
+        public Feedback(string title, string description, int rating)
+            : base(title, description)
+        {
+            Rating = rating;
+            Status =  FeedbackStatusType.New;
+        }
+
         public int Rating 
         {
             get => rating;
@@ -25,17 +33,56 @@ namespace TaskManager.Models
             }
         }
 
-        public FeedbackStatusType Status => throw new NotImplementedException();
+        public FeedbackStatusType Status { get; private set; }
         
         public void AdvanceFeedbackStatus()
         {
-            //Виж от кой статус стартираш и до къде върти
-        } 
-        
-        public void RevertFeedbackStatus()
-        {
-            //До кой статус стига
+            switch (Status)
+            {
+                case FeedbackStatusType.New:
+                    Status = FeedbackStatusType.Unscheduled;
+                    AddToChangeHistory($"Status changed from {FeedbackStatusType.New} to {FeedbackStatusType.Unscheduled}");
+                    break;
+                case FeedbackStatusType.Unscheduled:
+                    Status = FeedbackStatusType.Scheduled;
+                    AddToChangeHistory($"Status changed from {FeedbackStatusType.Unscheduled} to {FeedbackStatusType.Scheduled}");
+                    break;
+                case FeedbackStatusType.Scheduled:
+                    Status = FeedbackStatusType.Done;
+                    AddToChangeHistory($"Status changed from {FeedbackStatusType.Scheduled} to {FeedbackStatusType.Done}");
+                    break;
+                case FeedbackStatusType.Done:
+                    string message = $"Status is already at Done, can't advance any further";
+                    AddToChangeHistory(message);
+                    throw new ArgumentException(message);
+                default:
+                    throw new ArgumentException($"Feedbag status can only be one of the following: New, Unscheduled, Scheduled, Done");
+            }
         }
 
+        public void RevertFeedbackStatus()
+        {
+            switch (Status)
+            {
+                case FeedbackStatusType.Done:
+                    Status = FeedbackStatusType.Scheduled;
+                    AddToChangeHistory($"Status changed from {FeedbackStatusType.Done} to {FeedbackStatusType.Scheduled}");
+                    break;
+                case FeedbackStatusType.Scheduled:
+                    Status = FeedbackStatusType.Unscheduled;
+                    AddToChangeHistory($"Status changed from {FeedbackStatusType.Scheduled} to {FeedbackStatusType.Unscheduled}");
+                    break;
+                case FeedbackStatusType.Unscheduled:
+                    Status = FeedbackStatusType.New;
+                    AddToChangeHistory($"Status changed from {FeedbackStatusType.Unscheduled} to {FeedbackStatusType.New}");
+                    break;
+                case FeedbackStatusType.New:
+                    string message = $"Status is already at New, can't revert any further";
+                    AddToChangeHistory(message);
+                    throw new ArgumentException(message);
+                default:
+                    throw new ArgumentException($"Feedbag status can only be one of the following: New, Unscheduled, Scheduled, Done");
+            }
+        }
     }
 }
