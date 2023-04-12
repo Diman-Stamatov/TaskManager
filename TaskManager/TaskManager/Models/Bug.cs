8e7b7fa2d03d32aa797ba595ee155c8f6acc2321
Plamen Draganov <plamen.draganov@gmail.com>
2023-04-12 10:19:56 +0300

    upgrade Bug

--- TaskManager/TaskManager/Models/Bug.cs
+++ TaskManager/TaskManager/Models/Bug.cs
@@ -87,8 +87,8 @@ namespace TaskManager.Models
 
         public override void AdvanceStatus()
         {
-            var type = priority.GetType();
-            int currentValue = (int)Priority;
+            var type = status.GetType();
+            int currentValue = (int)Status;
             string propertyName = GetMethodName().TrimAdvance();
 
             ValidateAdvanceMethod(type, currentValue, propertyName);
@@ -98,8 +98,8 @@ namespace TaskManager.Models
 
         public override void RevertStatus()
         {
-            var type = priority.GetType();
-            int currentValue = (int)Priority;
+            var type = status.GetType();
+            int currentValue = (int)Status;
             string propertyName = GetMethodName().TrimRevert();
 
             ValidateRevertMethod(type, currentValue, propertyName);
@@ -109,5 +109,24 @@ namespace TaskManager.Models
         public IList<string> StepsToReproduce { get => new List<string>(stepsToReproduce); }
 
         public BugStatusType StatusType => throw new NotImplementedException();
+        public override string ToString()
+        {
+            StringBuilder bugInfo = new StringBuilder();
+            int number = 1;
+            bugInfo.Append(base.ToString());
+            bugInfo.AppendLine($"List of steps to reproduce it:");
+            bugInfo.AppendLine(StringGenerator('=', 10));
+            foreach (var step in stepsToReproduce)
+            {
+                bugInfo.AppendLine($"{number++}. {step}");
+                bugInfo.AppendLine(StringGenerator('=', 10));
+            }
+            bugInfo.AppendLine(StringGenerator('=', 10));
+            bugInfo.AppendLine($"Priority: {Priority}");
+            bugInfo.AppendLine($"Saverity: {Severity}");
+            bugInfo.AppendLine($"Status: {Status}");
+            bugInfo.AppendLine($"Assigned to: {Assignee.Name}");
+            return bugInfo.ToString().Trim();
+        }
     }
 }
