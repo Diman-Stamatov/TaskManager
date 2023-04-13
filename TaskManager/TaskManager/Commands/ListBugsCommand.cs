fefe5b2a6180a5d969f251cdad5a8607efffdbc3
Meli <melidragon@gmail.com>
2023-04-13 10:08:18 +0300

    ListBugsCommand updated

--- TaskManager/TaskManager/Commands/ListBugsCommand.cs
+++ TaskManager/TaskManager/Commands/ListBugsCommand.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using TaskManager.Core.Interfaces;
 using TaskManager.Models.Enums;
 using TaskManager.Models;
+using TaskManager.Models.Contracts;
+using TaskManager.Exceptions;
 
 namespace TaskManager.Commands
 {
@@ -27,35 +29,73 @@ namespace TaskManager.Commands
 
             if (filterByCommand == "FilterActive")
             {
-                List<Bug> bugsActive = task.
-               Where(bug => bug.Status == BugStatusType.Active).
-               OrderBy(bug => bug.Title).
-               ThenBy(bug => bug.Priority == PriorityType.High).
-               ThenBy(bug => bug.Priority == PriorityType.Medium).
-               ThenBy(bug => bug.Priority == PriorityType.Low).
-               ThenBy(bug => bug.Severity == SeverityType.Critical).
-               ThenBy(bug => bug.Severity == SeverityType.Major).
-               ThenBy(bug => bug.Severity == SeverityType.Minor).
-               ToList();
-               
+                List<Bug> bugsActive = null;
+                switch (sortByCommand)
+                {
+                    case "SortedTitle":
+                    bugsActive = task.
+                    Where(bug => bug.Status == BugStatusType.Active).
+                    OrderBy(bug => bug.Title).ToList();;
+                        break;
+                    case "SortedPriority":
+                    bugsActive = task.
+                    Where(bug => bug.Status == BugStatusType.Active).
+                    OrderBy(bug => bug.Priority == PriorityType.High).
+                    ThenBy(bug => bug.Priority == PriorityType.Medium).
+                    ThenBy(bug => bug.Priority == PriorityType.Low).ToList();
+                        break;
+                    case "SortedSeverity":
+                        bugsActive = task.
+                    Where(bug => bug.Status == BugStatusType.Active).
+                    OrderBy(bug => bug.Severity == SeverityType.Critical).
+                    ThenBy(bug => bug.Severity == SeverityType.Major).
+                    ThenBy(bug => bug.Severity == SeverityType.Minor).
+                    ToList();
+                        break;
+                }
+
+                if (bugsActive == null)
+                {
+                    throw new InvalidUserInputException("The filtering or sorting commands are incorrect.");
+                }
+
                 foreach (Bug bug in bugsActive)
                 {
                     stringBuilder.Append(bug);
                     StringGenerator('*', 15);
                 }
             }
-            else if (filterByCommand == "Fixed")
+            else if (filterByCommand == "FilterFixed")
             {
-                List<Bug> bugsFixed = task.
-               Where(bug => bug.Status == BugStatusType.Fixed).
-               OrderBy(bug => bug.Title).
-               ThenBy(bug => bug.Priority == PriorityType.High).
-               ThenBy(bug => bug.Priority == PriorityType.Medium).
-               ThenBy(bug => bug.Priority == PriorityType.Low).
-               ThenBy(bug => bug.Severity == SeverityType.Critical).
-               ThenBy(bug => bug.Severity == SeverityType.Major).
-               ThenBy(bug => bug.Severity == SeverityType.Minor).
-               ToList();
+                List<Bug> bugsFixed = null;
+                switch (sortByCommand)
+                {
+                    case "SortedTitle":
+                        bugsFixed = task.
+                        Where(bug => bug.Status == BugStatusType.Fixed).
+                        OrderBy(bug => bug.Title).ToList(); ;
+                        break;
+                    case "SortedPriority":
+                        bugsFixed = task.
+                        Where(bug => bug.Status == BugStatusType.Fixed).
+                        OrderBy(bug => bug.Priority == PriorityType.High).
+                        ThenBy(bug => bug.Priority == PriorityType.Medium).
+                        ThenBy(bug => bug.Priority == PriorityType.Low).ToList();
+                        break;
+                    case "SortedSeverity":
+                        bugsFixed = task.
+                    Where(bug => bug.Status == BugStatusType.Fixed).
+                    OrderBy(bug => bug.Severity == SeverityType.Critical).
+                    ThenBy(bug => bug.Severity == SeverityType.Major).
+                    ThenBy(bug => bug.Severity == SeverityType.Minor).
+                    ToList();
+                        break;
+                }
+
+                if (bugsFixed == null)
+                {
+                    throw new InvalidUserInputException("The filtering or sorting commands are incorrect.");
+                }
 
                 foreach (Bug bug in bugsFixed)
                 {
@@ -63,21 +103,41 @@ namespace TaskManager.Commands
                     StringGenerator('*', 15);
                 }
 
-            }
-            else if(filterByCommand == "Assignee")
-            {
-                List<Bug> bugsAssign = task.
-               Where(bug => bug.Assignee.IsAssignedToATeam).
-               OrderBy(bug => bug.Title).
-               ThenBy(bug => bug.Priority == PriorityType.High).
-               ThenBy(bug => bug.Priority == PriorityType.Medium).
-               ThenBy(bug => bug.Priority == PriorityType.Low).
-               ThenBy(bug => bug.Severity == SeverityType.Critical).
-               ThenBy(bug => bug.Severity == SeverityType.Major).
-               ThenBy(bug => bug.Severity == SeverityType.Minor).
-               ToList();
 
-                foreach (Bug bug in bugsAssign)
+            }
+            else if(filterByCommand == "FilterAssignee")
+            {
+                List<Bug> bugsAssignee = null;
+                switch (sortByCommand)
+                {
+                    case "SortedTitle":
+                        bugsAssignee = task.
+                        Where(bug => bug.Assignee.IsAssignedToATeam).
+                        OrderBy(bug => bug.Title).ToList(); ;
+                        break;
+                    case "SortedPriority":
+                        bugsAssignee = task.
+                        Where(bug => bug.Assignee.IsAssignedToATeam).
+                        OrderBy(bug => bug.Priority == PriorityType.High).
+                        ThenBy(bug => bug.Priority == PriorityType.Medium).
+                        ThenBy(bug => bug.Priority == PriorityType.Low).ToList();
+                        break;
+                    case "SortedSeverity":
+                        bugsAssignee = task.
+                    Where(bug => bug.Assignee.IsAssignedToATeam).
+                    OrderBy(bug => bug.Severity == SeverityType.Critical).
+                    ThenBy(bug => bug.Severity == SeverityType.Major).
+                    ThenBy(bug => bug.Severity == SeverityType.Minor).
+                    ToList();
+                        break;
+                }
+
+                if (bugsAssignee == null)
+                {
+                    throw new InvalidUserInputException("The filtering or sorting commands are incorrect.");
+                }
+
+                foreach (Bug bug in bugsAssignee)
                 {
                     stringBuilder.Append(bug);
                     StringGenerator('*', 15);
