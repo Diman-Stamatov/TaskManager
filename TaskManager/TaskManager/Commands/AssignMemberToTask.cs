125021c682b06196e637d89b0b9aae4d4580de19
Plamen Draganov <plamen.draganov@gmail.com>
2023-04-13 18:55:21 +0300

    changes

--- TaskManager/TaskManager/Commands/AssignMemberToTask.cs
+++ TaskManager/TaskManager/Commands/AssignMemberToTask.cs
@@ -29,9 +29,7 @@ namespace TaskManager.Commands
         }
 
         private string AddMemberToTask(int taskName, string memberName)
-        {
-            IMember member = null;
-            member.fi
+        {            
             var foundTeam = Repository.GetTask(taskName);
             IMember foundMember = Repository.GetMember(memberName);
             if (foundMember.IsAssignedToATeam == true)
