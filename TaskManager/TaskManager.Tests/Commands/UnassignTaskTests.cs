816593f10b72f41b257299ed7a69e8a2adcc1d54
Plamen Draganov <plamen.draganov@gmail.com>
2023-04-20 06:24:26 +0300

    Add UnassignTaskCommand Tests - ToDo - просто стана много късно/рано

--- TaskManager/TaskManager.Tests/Commands/UnassignTaskTests.cs
+++ TaskManager/TaskManager.Tests/Commands/UnassignTaskTests.cs
@@ -3,11 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Core.Interfaces;
+using TaskManager.Core;
+using TaskManager.Exceptions;
 
 namespace TaskManager.Tests.Commands
 {
     [TestClass]
     public class UnassignTaskTests
     {
+        private IRepository repository;
+        private ICommandFactory commandFactory;
+        private IMember mockMember;
+        private IStory mockStory;
+        private IBug mockBug;
+        private IFeedback mockFeedback;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            repository = new Repository();
+            commandFactory = new CommandFactory(repository);
+            mockBug = repository.CreateBug(ValidTaskTitle, ValidDescription, ValidPriority, ValidSeverity);
+            mockStory = repository.CreateStory(ValidTaskTitle, ValidDescription, ValidPriority, ValidSize);
+            mockFeedback = repository.CreateFeedback(ValidTaskTitle, ValidDescription, ValidId);
+            mockMember = repository.CreateMember(ValidMemberName);
+           
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUserInputException))]
+        public void CommandShouldThrow_When_ArgumentsCountIsInvalid()
+        {
+            ICommand command = commandFactory.Create("UnassignTask");
+            command.Execute();
+        }
+
+        //ToDo
+        //[TestMethod]
+        //public void CommandShouldThrow_When_IDIsInvalid()
+        //{
+        //    mockBug.Assign(mockMember);
+        //    ICommand command = commandFactory.Create($"UnassignTask {ValidMemberName}");
+        //    command.Execute();
+        //    Assert.IsTrue(repository.MemberExists(ValidMemberName));
+        //}
     }
 }
