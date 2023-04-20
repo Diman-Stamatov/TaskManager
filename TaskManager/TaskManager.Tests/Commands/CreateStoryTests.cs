0daf06012fce4ad4a922d95bed633e061ff95bee
Plamen Draganov <plamen.draganov@gmail.com>
2023-04-20 05:45:33 +0300

    Add CreateStoryCommand Tests

--- TaskManager/TaskManager.Tests/Commands/CreateStoryTests.cs
+++ TaskManager/TaskManager.Tests/Commands/CreateStoryTests.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Core.Interfaces;
+using TaskManager.Core;
+using TaskManager.Exceptions;
 
 namespace TaskManager.Tests.Commands
 {
     [TestClass]
     public class CreateStoryTests
     {
+        private IRepository repository;
+        private ICommandFactory commandFactory;
+        private ITeam mockTeam;
+        private IMember mockMember;
+        private IStory mockStory;
+        private IBug mockBug;
+        private IFeedback mockFeedback;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            repository = new Repository();
+            commandFactory = new CommandFactory(repository);
+            mockStory = repository.CreateStory(ValidTaskTitle, ValidDescription, ValidPriority, ValidSize);
+            mockBug = repository.CreateBug(ValidTaskTitle, ValidDescription, ValidPriority, ValidSeverity);
+            mockFeedback = repository.CreateFeedback(ValidTaskTitle, ValidDescription, ValidId);
+            mockMember = repository.CreateMember(ValidMemberName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUserInputException))]
+        public void CommandShouldThrow_When_ArgumentsCountIsInvalid()
+        {
+            ICommand command = commandFactory.Create("CreateStory");
+            command.Execute();
+        }
+
+        [TestMethod]
+        public void CommandShouldCreateFeedback_WhenInputIsValid()
+        {
+            ICommand command = commandFactory.Create($"CreateStory {ValidTaskTitle} {ValidDescription} {ValidPriority} {ValidSize} ");
+            command.Execute();
+            Assert.IsTrue(repository.GetTask(1).Id == 1);
+        }
     }
 }
