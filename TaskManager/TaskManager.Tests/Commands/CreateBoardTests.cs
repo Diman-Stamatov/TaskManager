94347a9ac224e387bfd96c23dec54783fdd3149e
Plamen Draganov <plamen.draganov@gmail.com>
2023-04-20 05:38:33 +0300

    Add CreateBugCommand Tests

--- TaskManager/TaskManager.Tests/Commands/CreateBoardTests.cs
+++ TaskManager/TaskManager.Tests/Commands/CreateBoardTests.cs
@@ -3,11 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Core;
+using TaskManager.Core.Interfaces;
+using TaskManager.Exceptions;
 
 namespace TaskManager.Tests.Commands
 {
     [TestClass]
     public class CreateBoardTests
     {
+        private IRepository repository;
+        private ICommandFactory commandFactory;
+        private ITeam mockTeam;
+        private IMember mockMember;
+        private IStory mockStory;
+        private IBug mockBug;
+        private IFeedback mockFeedback;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            repository = new Repository();
+            commandFactory = new CommandFactory(repository);
+            mockTeam = repository.CreateTeam(ValidTeamName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUserInputException))]
+        public void CommandShouldThrow_When_ArgumentsCountIsInvalid()
+        {
+            ICommand command = commandFactory.Create("CreateBoard");
+            command.Execute();
+        }
+
+        [TestMethod]
+        public void CommandShouldCreate_When_InputIsValid()
+        {
+            ICommand command = commandFactory.Create($"CreateBoard {ValidTeamName} {ValidBoardName}");
+            command.Execute();
+            Assert.IsTrue(repository.TeamExists(ValidTeamName));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EntryNotFoundException))]
+        public void CommandShouldThrow_When_TeamIsInvalid()
+        {
+            ICommand command = commandFactory.Create($"CreateBoard invalidTeamName {ValidBoardName}");
+            command.Execute();
+        }
     }
 }
--- TaskManager/TaskManager.Tests/Commands/CreateBugTests.cs
+++ TaskManager/TaskManager.Tests/Commands/CreateBugTests.cs
@@ -1,13 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TaskManager.Core;
+using TaskManager.Core.Interfaces;
+using TaskManager.Exceptions;
 
 namespace TaskManager.Tests.Commands
 {
     [TestClass]
     public class CreateBugTests
     {
+        private IRepository repository;
+        private ICommandFactory commandFactory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            repository = new Repository();
+            commandFactory = new CommandFactory(repository);
+            mockStory = repository.CreateStory(ValidTaskTitle, ValidDescription, ValidPriority, ValidSize);
+            mockBug = repository.CreateBug(ValidTaskTitle, ValidDescription, ValidPriority, ValidSeverity);
+            mockFeedback = repository.CreateFeedback(ValidTaskTitle, ValidDescription, ValidId);
+            mockMember = repository.CreateMember(ValidMemberName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUserInputException))]
+        public void CommandShouldThrow_When_ArgumentsCountIsInvalid()
+        {
+            ICommand command = commandFactory.Create("CreateBug");
+            command.Execute();
+        }
+
+        [TestMethod]
+        public void CommandShouldCreateBug_WhenInputIsValid()
+        {
+            ICommand command = commandFactory.Create($"CreateBug {ValidTaskTitle} {ValidDescription} {ValidPriority} {ValidSeverity}");
+            command.Execute();
+            Assert.IsTrue(repository.GetTask(1).Id == 1);
+        }
     }
 }
